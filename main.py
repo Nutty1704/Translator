279339a632dcae729a891798d61a0fc88565687b
Nutty1704 <abhijitupadhyay03@gmail.com>
2023-05-07 18:51:33 +1000

    Add WordTranslater to main interface

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from text import TextTranslator
+from word import WordTranslator
+import time
 
 class Main:
     def __init__(self) -> None:
@@ -7,15 +9,20 @@ class Main:
     def run(self):
         while True:
             print('1. Translate a text file')
-            print('2. Exit')
+            print('2. Translate a Word document')
+            print('3. Exit')
             choice = input('Enter your choice: ')
 
             if choice == '1':
                 TextTranslator()
             elif choice == '2':
+                WordTranslator()
+            elif choice == '3':
                 break
             else:
                 print('Invalid choice. Try again.')
+
+            time.sleep(1)
 
 
 if __name__ == "__main__":
